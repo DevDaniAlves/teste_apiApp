8920865611d6aefe38ccfef3b49bdcfb37b478d6
Daniel Alves Santos <82129927+DevDaniAlves@users.noreply.github.com>
2023-08-20 01:37:46 -0400

    Update index.js

--- src/database/index.js
+++ src/database/index.js
@@ -1,14 +1,17 @@
 const { Sequelize } = require('sequelize');
 const configDatabase = require('./config');
 
-const { MerendeiraModel } = require('../models/merendeira-model');
-const { NutricionistaModel } = require('../models/nutricionista-model');
-const { NotaFiscalModel } = require('../models/nota-fiscal-mode');
+const { Animal } = require('./database/models/animais');
+const { Entrada } = require('./database/models/entrada');
+const { Movimentacao } = require('./database/models/movimentacao');
+const { Pasto } =  require('./database/models/pastos');
+const { User } = require('./database/models/user');
 
 const database = new Sequelize(configDatabase);
 
-MerendeiraModel.init(database);
-NutricionistaModel.init(database);
-NotaFiscalModel.init(database);
-
+Animal.init(database);
+Entrada.init(database);
+Movimentacao.init(database);
+Pasto.init(database);
+User.init(database);
 module.exports = database;
